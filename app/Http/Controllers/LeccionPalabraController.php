4fb4b04371e638084a868556a783671efc26513f
Richard Taicus Moriano <richardtmoriano94@gmail.com>
2019-05-29 13:27:58 -0500

    Revert "Merge branch 'actualizar_tablas_db' of https://github.com/richymori/leinla into actualizar_tablas_db"
    This reverts commit dc557e270302cc3033832c7d3c97ffa92a57711e, reversing
    changes made to b7e351916905fcd8d7b85b962b6c18a044ce6e76.

---
+++ app/Http/Controllers/LeccionPalabraController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\LeccionPalabra;
+use Illuminate\Http\Request;
+
+class LeccionPalabraController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\LeccionPalabra  $leccionPalabra
+     * @return \Illuminate\Http\Response
+     */
+    public function show(LeccionPalabra $leccionPalabra)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\LeccionPalabra  $leccionPalabra
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(LeccionPalabra $leccionPalabra)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\LeccionPalabra  $leccionPalabra
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, LeccionPalabra $leccionPalabra)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\LeccionPalabra  $leccionPalabra
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(LeccionPalabra $leccionPalabra)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/LeccionPreguntaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\LeccionPregunta;
+use Illuminate\Http\Request;
+
+class LeccionPreguntaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\LeccionPregunta  $leccionPregunta
+     * @return \Illuminate\Http\Response
+     */
+    public function show(LeccionPregunta $leccionPregunta)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\LeccionPregunta  $leccionPregunta
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(LeccionPregunta $leccionPregunta)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\LeccionPregunta  $leccionPregunta
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, LeccionPregunta $leccionPregunta)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\LeccionPregunta  $leccionPregunta
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(LeccionPregunta $leccionPregunta)
+    {
+        //
+    }
+}
