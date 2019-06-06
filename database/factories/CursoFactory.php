8d3dd604ba19b11ac547773df75c5881cd4ed8ea
Richard Taicus Moriano <richardtmoriano94@gmail.com>
2019-06-05 21:55:50 -0500

    curso factory

--- database/factories/CursoFactory.php
+++ database/factories/CursoFactory.php
@@ -4,9 +4,21 @@
 
 use App\Curso;
 use Faker\Generator as Faker;
+use App\Lengua;
+use App\Desarrollador;
 
 $factory->define(Curso::class, function (Faker $faker) {
     return [
-        //
+        
+        'curso' => $faker->curso,
+
+        'lengua_id' => function () {
+            return factory(Lengua::class)->create()->id;
+        },
+        'desarrollador_id' => function () {
+            return factory(Desarrollador::class)->create()->id;
+        },
+
+
     ];
 });
